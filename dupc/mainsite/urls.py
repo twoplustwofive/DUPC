5523bb73da3963818eaffeed754f5219dfe1b8e1
twoplustwofive <rathod18@iitg.ac.in>
2020-06-05 21:48:15 +0530

    login/logout page

--- dupc/mainsite/urls.py
+++ dupc/mainsite/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.homepage,name='homepage'),
+    path('login',views.loginpage,name='loginpage'),
+    path('logout',views.Logout,name='logoutpage'),
 ]
