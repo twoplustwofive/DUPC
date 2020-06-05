89bd78d72d67c982b6ea5ea53600b0b2ed1edbc4
twoplustwofive <rathod18@iitg.ac.in>
2020-06-05 11:07:27 +0530

    static addition

--- dupc/mainsite/urls.py
+++ dupc/mainsite/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('mainsite.urls')),
+    path('',views.homepage,name='homepage'),
 ]
