69be29be22ef06d9f32a2d2a0aa530ca0ea5d228
Curious-Ujjwal <ujjwal24111999@gmail.com>
2020-07-16 19:55:03 +0530

    Added Publication under apps and in main urls file

--- dupc/dupc/urls.py
+++ dupc/dupc/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mainsite.urls',namespace='mainsite')),
     path('account/',include('account.urls',namespace='account')),
+    path('publication/',include('publication.urls',namespace='publication')),
     path('news_and_updates/',include('news.urls',namespace = 'news')),
     path('outreach/',include('outreach.urls',namespace = 'outreach')),
-    
 ]+  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
