a09a9175f59ae4024bda07e3bb491c1ea1035536
kaustubhshukla30 <kaustubhshukla30@gmail.com>
2020-07-19 21:18:13 +0530

    minnor changes

--- dupc/account/urls.py
+++ dupc/account/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from .import views
 app_name = "account"
 
 urlpatterns = [
     path('',views.loginpage,name='loginpage'),
     path('logout/',views.Logout,name='logoutpage'),
+    
+    
 ]+  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- dupc/dupc/urls.py
+++ dupc/dupc/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mainsite.urls',namespace='mainsite')),
     path('account/',include('account.urls',namespace='account')),
-
+    
     path('publication/',include('publication.urls',namespace='publication')),
     path('news_and_updates/',include('news.urls',namespace = 'news')),
     path('outreach/',include('outreach.urls',namespace = 'outreach')),
