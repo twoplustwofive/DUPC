fbdcbd39d97e16a62f0037b6e6e9248b487f1463
Curious-Ujjwal <ujjwal24111999@gmail.com>
2020-08-04 21:19:51 +0530

    blank field modified for frontend

--- dupc/mainsite/models.py
+++ dupc/mainsite/models.py
@@ -6,7 +6,7 @@ class team(models.Model):
     name = models.CharField(max_length = 100)
     image = models.ImageField(upload_to = 'images/', default = 'download.png')
     designation = models.CharField(max_length = 1500, default= None)
-    institute_name = models.CharField(max_length = 1500,default = 'IITG')
+    institute_name = models.CharField(max_length = 1500,default = 'IITG',blank=True)
     department = models.CharField(max_length=1500,default = 'HSS')
     information = models.TextField(max_length = 3000, default= None)
     
