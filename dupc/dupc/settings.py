cdde1c17622e62bc28d15bf9fd29cff6fca9957c
kaustubhshukla30 <kaustubhshukla30@gmail.com>
2020-08-08 16:38:42 +0530

    password done

--- dupc/dupc/settings.py
+++ dupc/dupc/settings.py
@@ -135,11 +135,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = '#'
+EMAIL_HOST_USER = 'water.diplomacy.in.brahmaputra@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = '#'
+EMAIL_HOST_PASSWORD = 'dupc@123'
 
 LOGIN_REDIRECT_URL = 'accounts/login'
 LOGOUT_REDIRECT_URL = '/account/logout'
