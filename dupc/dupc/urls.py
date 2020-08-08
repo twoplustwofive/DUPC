73d898c1b2a1325a8eaefcd808777b0c466ef6c3
kaustubhshukla30 <kaustubhshukla30@gmail.com>
2020-08-08 23:07:56 +0530

    final bugs removed

--- dupc/dupc/urls.py
+++ dupc/dupc/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from django.conf import settings
 from .import views
 from django.contrib.auth import views as auth_views
